222edac266e1bbe407f1e2c7604c1ae59389e25d
andalibkibria <andalib.kibria@gmail.com>
2018-01-23 19:01:10 -0500

    Create components folder

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export default class DrawerUndockedExample extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
+
+  render() {
+    return (
+      <div className="navmenu">
+        <RaisedButton
+          label="SL Hearns"
+          onClick={this.handleToggle}
+        />
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem onClick={this.handleClose}>Home</MenuItem>
+          <MenuItem onClick={this.handleClose}>Portfolio</MenuItem>
+          <MenuItem onClick={this.handleClose}>Questionnaire</MenuItem>
+          <MenuItem onClick={this.handleClose}>Contact</MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
+}
