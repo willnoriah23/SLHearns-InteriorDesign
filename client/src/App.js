19e9f54945cd8fca88ddabb6d01c2f681588aeba
andalibkibria <andalib.kibria@gmail.com>
2018-01-23 21:04:42 -0500

    WIP: Error resolved

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Questionaire from "./pages/Questionaire";
 import Portfolio from "./pages/Portfolio";
+// import Transition from "./pages/Transition";
 
 const App = () =>
 
@@ -12,6 +13,8 @@ const App = () =>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/questions" component={Questionaire} />
           <Route exact path="/portfolio" component={Portfolio} />
+          {/*<Route exact path="/transition" component={Transition} />*/}
+
           {/*<Route component={NoMatch} />*/}
         </Switch>
       </div>
---
+++ client/src/pages/Questionaire/Questionaire.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import KitchenQ from '../../components/Questions/KitchenQues';
+import BathroomQ from '../../components/Questions/BathroomQ';
+import SubmitButton from "../../components/Button/Submitbutton";
+import Nav from "../../components/Nav/Navbar";
+import "./Questionaire.css";
+import Logo from "../../components/Logo";
+
+
+class Questionaire extends Component {
+
+    state = [
+        {
+            field1: "",
+            field2: "",
+            field3: ""
+
+
+        }
+    ];
+
+    handleChange1 = (event, index, value) => this.setState({field1: value});
+
+    handleChange2 = (event, index, value) => this.setState({field2: value});
+
+    handleChange3 = (event, index, value) => this.setState({field3: value});
+    handlesubmit = (event, index, value) => {
+        // write code for submitting code
+    };
+
+
+    render() {
+        return (
+            <MuiThemeProvider>
+                <Nav />
+                <Logo />
+
+                <section className='app'>
+                <TextField
+                    hintText="Name"
+                    name={"name"}
+                    id={"name"}
+                    floatingLabelText="Name"
+                    errorText="Please enter your name."
+                    fullWidth={true}
+                /><br/>
+                <TextField
+                    hintText="Email"
+                    name="email"
+                    id="email"
+                    floatingLabelText="Email"
+                    errorText="Please enter your email address."
+                    fullWidth={true}
+                /><br/>
+                <TextField
+                    hintText="Phone Number"
+                    name="phonenum"
+                    id="phonenum"
+                    errorText="Please enter your phone number."
+                    fullWidth={true}
+                /><br/>
+                <SelectField
+                    floatingLabelText="Budget"
+                    value={this.state.field1}
+                    onChange={this.handleChange1}
+                    fullWidth={true}
+                >
+                    <MenuItem value={1} primaryText="$5,000 - $10,000" />
+                    <MenuItem value={2} primaryText="$10,001 - $15,000" />
+                    <MenuItem value={3} primaryText="$15,001 - $20,000" />
+                    <MenuItem value={4} primaryText="$20,000+" />
+                </SelectField>
+                <br />
+
+                <SelectField
+                    floatingLabelText="Room:"
+                    value={this.state.field2}
+                    onChange={this.handleChange2}
+                    id="roomtype"
+                    fullWidth={true}
+                >
+                    <MenuItem value={1} primaryText="Bath" />
+                    <MenuItem value={2} primaryText="Kitchen" />
+                </SelectField>
+                {/*<KitchenQ/>*/}
+                {this.state.field2 === 1 && <BathroomQ fullWidth={true} value={this.state.field3} handlechange={this.handleChange3}/>}
+                {this.state.field2 === 2 && <KitchenQ fullWidth={true}/>}
+
+                {/*add if statemetn to render different questions*/}
+                <br />
+
+                <TextField
+                    hintText="Family Size"
+                    name="famsize"
+                    id="famsize"
+                    floatingLabelText="Family Size"
+                    errorText="Please provide the size of your family."
+                    fullWidth={true}
+                /><br/>
+
+                <TextField
+                    hintText="Please provide three(3) things you love about the current design."
+                    name="love3"
+                    id="love3"
+                    floatingLabelText="Three things you love."
+                    errorText="Please provide at least one thing you currently love."
+                    multiLine={true}
+                    rows={1}
+                    rowsMax={4}
+                    fullWidth={true}
+
+                /><br/>
+                <TextField
+                    hintText="Please provide three(3) things you do not love about the current design."
+                    name="dontlove3"
+                    id="dontlove3"
+                    floatingLabelText="Three things you do not love."
+                    errorText="Please provide at least one thing you currently do not love."
+                    multiLine={true}
+                    rows={1}
+                    rowsMax={4}
+                    fullWidth={true}
+                /><br/>
+
+                <SubmitButton handlesubmit={this.handlesubmit} name={"Submit"}>
+                </SubmitButton>
+
+            </section>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+export default Questionaire;
