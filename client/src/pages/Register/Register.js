c54260b0aba300100d4e6b4cf32ed60865f7c30a
Ari Vidal <ariel.m.vidal@gmail.com>
2018-02-03 00:13:39 -0500

    fixing some functions in the register page

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
+import Logo from "../../components/Logo";
+import Nav from "../../components/Nav";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import "./Register.css";
 
 class Login extends React.Component {
   constructor () {
@@ -34,28 +38,36 @@ class Login extends React.Component {
   render() {
     return (
       <div>
-        <h1>Registration Page</h1>
-        <form>
+        <MuiThemeProvider>
+        <Nav/>
+        <Logo/>
+        <h1>Registration</h1>
+        <form className={"form"}>
           <input
+          id="fullname"
           type="text"
           name="fullname"
+          placeholder="Name"
           onChange={this.getValue} />
           <br />
 
           <input
           type="email"
           name="email"
+          placeholder="Email"
           onChange={this.getValue} />
           <br />
 
           <input
           type="password"
           name="password"
+          placeholder="Password"
           onChange={this.getValue} />
           <br />
 
           <Link to="/register"><button onClick={this.sendData}>Login</button></Link>
         </form>
+        </MuiThemeProvider>
       </div>
     )
   }
