27dfe29603f705d1766e22a51d2cb64be4c8f76d
Noriah Williams <willnoriah23@gmail.com>
2018-02-03 00:11:57 -0500

    Styling changes to questionnaire page

--- client/src/Blurb/Blurb.js
+++ client/src/Blurb/Blurb.js
@@ -1,10 +1,10 @@
 export default [
     "Traditional:" +
-    "\nClassic European decor with deep, warm, rich tones.  Features of architectural details featuring symmetrical lines.  May have curves, but not ornate or ostentatious.",
+    "Classic European decor with deep, warm, rich tones.  Features of architectural details featuring symmetrical lines.  May have curves, but not ornate or ostentatious.",
     
-    "Transitional: \n" +
+    "Transitional:" +
     "The marriage of traditional and contemporary resulting in a clean, classic, timeless design.",
 
-    "Contemporary: \n" +
+    "Contemporary:" +
     "Neutral Elements with accents of color.  Featuring basics of clean lines, with shape and form.  Contemporary design relies on materials to give a space visual interest.  Simplicity, subtle sophistication, and textures where less is more."
 ];
